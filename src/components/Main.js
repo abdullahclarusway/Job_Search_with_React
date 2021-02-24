a95d5395a7da47e6a5de119d5b14ca5fb6719a7a
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-02-24 22:54:29 +0100

    Add categories to main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Template from "../onepirate/Home";
+import axios from "axios";
+import Blog from "../blog/Blog";
 
 function Main() {
-  return <div>Main</div>;
+  // const fetchData = async () => {
+  //   const response = await axios
+  //     .get("https://remotive.io/api/remote-jobs?category=software-dev")
+  //     .then((res) => console.log(res.data.jobs))
+  //     .catch((err) => console.log(err));
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  return (
+    <div>
+      <Blog />
+    </div>
+  );
 }
 
 export default Main;
