5d78c45bd19f67f6b229d012647ba1bc75459447
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-02-24 22:53:32 +0100

    Fix bug

--- src/firebase/Firebase.utils.js
+++ src/firebase/Firebase.utils.js
@@ -2,24 +2,20 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import { customErrorHandler } from "../helper/customErrorHandler";
 //firestore
-
-const devConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
+const firebaseConfig = {
+  apiKey: "AIzaSyAZSqisBoxkSJimMUWFx9ZCr4Hp7GvbNlY",
+  authDomain: "find-jobs-app.firebaseapp.com",
+  projectId: "find-jobs-app",
+  storageBucket: "find-jobs-app.appspot.com",
+  messagingSenderId: "153911193602",
+  appId: "1:153911193602:web:ea1eb9066d2a09fb1e771b",
 };
 
-const prodConfig = {};
-
-const config = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
 
 class Firebase {
   constructor() {
     //TODO: add initialize check :: if (firebase.apps.length === 0)
-    firebase.initializeApp(config);
+    firebase.initializeApp(firebaseConfig);
     console.log("firebase", firebase);
     this.firebaseAuth = firebase.auth();
   }
