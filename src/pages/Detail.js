aab70547b822fe65586193d1d23ed0c7cdca563d
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-02-25 23:52:45 +0100

    Add detail component

---
+++ src/pages/Detail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+function Detail() {
+  const location = useLocation();
+  const myparam = location.state.params;
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: myparam.data.description,
+      }}
+    ></div>
+  );
+}
+export default Detail;
