6d90a9b764ebfc18e2e38f150420b27ba342d958
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-03-01 22:46:58 +0100

    Add grid styles  to savedjobs

--- src/pages/SavedJobs.js
+++ src/pages/SavedJobs.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import JobCard from "../JobCard/JobCard";
+import { Grid, CircularProgress } from "@material-ui/core";
+import useStyles from "../JobCard/styles";
 
 const SavedJobs = () => {
+  const classes = useStyles();
   const [jobList, setJobList] = useState([]);
   const res = JSON.parse(localStorage.getItem("FAVORITES"));
   const len = res.length;
@@ -8,9 +12,16 @@ const SavedJobs = () => {
 
   return (
     <div>
-      {res.map((item) => (
-        <p>{item.title}</p>
-      ))}
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
+        {res.map((item) => (
+          <JobCard data={item} />
+        ))}
+      </Grid>
     </div>
   );
 };
