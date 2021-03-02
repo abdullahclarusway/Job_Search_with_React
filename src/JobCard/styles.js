a0260fea991e331829eea0167020c6890ada8d30
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-03-02 23:59:41 +0100

    Add some style

--- src/JobCard/styles.js
+++ src/JobCard/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export default makeStyles({
+export default makeStyles((theme) => ({
   media: {
     height: 0,
     paddingTop: "56.25%",
@@ -24,9 +24,9 @@ export default makeStyles({
     boxSizing: "border-box",
   },
   overlay2: {
-    position: "absolute",
-    top: "20px",
-    right: "20px",
+    // position: "absolute",
+    // bottom: "20px",
+    // right: "20px",
     color: "white",
     cursor: "pointer",
   },
@@ -36,14 +36,29 @@ export default makeStyles({
   details: {
     display: "flex",
     justifyContent: "flex-start",
-    margin: "20px",
+    margin: "10px",
   },
   title: {
-    padding: "0 16px",
+    padding: "0 8px",
   },
   cardActions: {
     padding: "0 16px 8px 16px",
     display: "flex",
     justifyContent: "space-between",
   },
-});
+  button: {
+    margin: theme.spacing(1),
+  },
+  title: {
+    marginLeft: 20,
+  },
+
+  header: {
+    margin: 10,
+    padding: 10,
+  },
+  header__title: {
+    color: "blue",
+    marginBottom: 5,
+  },
+}));
