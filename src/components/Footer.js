9e5803dcac863b37873cc70fc7d7c8aad8dfd236
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-03-04 23:37:42 +0100

    Add some info to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,57 @@
-import React from "react";
+import * as React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 
-function Footer() {
-  return <div>Footer</div>;
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Lighthouse All rights reserved
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    // marginTop: theme.spacing(8),
+    padding: theme.spacing(6, 0),
+  },
+}));
+
+function Footer(props) {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.footer}>
+      <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom>
+          Lighthouse
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Start your career with us
+        </Typography>
+        <Copyright />
+      </Container>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  description: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
 
 export default Footer;
